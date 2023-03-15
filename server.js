9ab8d54ff808de1ada4b43b6298e876629dff4b9
Alvaro <alvarorivgar@gmail.com>
2023-03-15 17:57:30 +0100

    fixed videocall functionality

--- server.js
+++ server.js
@@ -5,8 +5,34 @@ const PORT = process.env.PORT || 5005;
 
 // Videocall
 
-require("./middlewares/videocall.middlewares")
-
-app.listen(PORT, () => {
+const myServer = app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
+// const http = require("http")
+// const server = http.createServer(app)
+const { Server } = require("socket.io")
+
+const io = new Server(myServer, {
+  	cors: {
+  		origin: "http://localhost:3000",
+  	}
+  })
+  
+  io.on("connection", (socket) => {
+    socket.emit("me", socket.id)
+    console.log("hola patata");
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+
